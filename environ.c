4986d606c508b602fe497f2ae8af43b96dfb54ad
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2022-12-21 10:51:51 -0800

    Tawasoul

--- environ.c
+++ environ.c
@@ -5,13 +5,12 @@
  * @info: Structure containing potential arguments. Used to maintain
  * constant function prototype.
  * Return: Always 0
- **/
+ */
 int _myenv(info_t *info)
 {
 	print_list_str(info->env);
 	return (0);
 }
-
 /**
  * _getenv - gets the value of an environ variable
  * @info: Structure containing potential arguments. Used to maintain
@@ -33,7 +32,6 @@ char *_getenv(info_t *info, const char *name)
 	}
 	return (NULL);
 }
-
 /**
  * _mysetenv - Initialize a new environment variable,
  * or modify an existing one
@@ -47,12 +45,11 @@ int _mysetenv(info_t *info)
 	{
 		_eputs("Incorrect number of arguements\n");
 		return (1);
-										}
+	}
 	if (_setenv(info, info->argv[1], info->argv[2]))
 		return (0);
 	return (1);
 }
-
 /**
  * _myunsetenv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
@@ -72,7 +69,6 @@ int _myunsetenv(info_t *info)
 		_unsetenv(info, info->argv[i]);
 	return (0);
 }
-
 /**
  * populate_env_list - populates env linked list
  * @info: Structure containing potential arguments. Used to maintain
@@ -89,4 +85,3 @@ int populate_env_list(info_t *info)
 	info->env = node;
 	return (0);
 }
-
