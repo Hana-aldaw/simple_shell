c4a9c514c1d03100c0a6bf3f10bc9c0dc54148bd
Tawasoulmohamedkhair <Tawasoulmohamedkhair@gmail.com>
2022-12-22 02:59:05 -0800

    Tawasoul

--- errors.c
+++ errors.c
@@ -1,52 +1,28 @@
 #include "shell.h"
 
 /**
-<<<<<<< HEAD
  *_eputs - prints an input string
  * @str: the string to be printed
  * Return: Nothing
  **/
 void _eputs(char *str)
-=======
- * _eputs - prints an input string
- * @str: the string to be printed
- *
- * Return: no return
- */
-void _eputs(char *str)
-
->>>>>>> 7b5aa13d41c8b74dd11e3fc12bbd5fdd4a300253
 {
 	int i = 0;
 
 	if (!str)
 		return;
-<<<<<<< HEAD
-=======
-
->>>>>>> 7b5aa13d41c8b74dd11e3fc12bbd5fdd4a300253
 	while (str[i] != '\0')
 	{
 		_eputchar(str[i]);
 		i++;
 	}
 }
-<<<<<<< HEAD
 /**
  * _eputchar - writes the character c to stderr
  * @c: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  **/
-=======
-
-/**
- * _eputchar - writes the character c to stderr
- * @c: The character to print
- *
- * Return: On success 1
- */
->>>>>>> 7b5aa13d41c8b74dd11e3fc12bbd5fdd4a300253
 int _eputchar(char c)
 {
 	static int i;
@@ -57,28 +33,17 @@ int _eputchar(char c)
 		write(2, buf, i);
 		i = 0;
 	}
-
 	if (c != BUF_FLUSH)
 		buf[i++] = c;
 	return (1);
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 7b5aa13d41c8b74dd11e3fc12bbd5fdd4a300253
 /**
  * _putfd - writes the character c to given fd
  * @c: The character to print
  * @fd: The filedescriptor to write to
-<<<<<<< HEAD
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  **/
-=======
- *
- * Return: On success 1
- */
->>>>>>> 7b5aa13d41c8b74dd11e3fc12bbd5fdd4a300253
 int _putfd(char c, int fd)
 {
 	static int i;
@@ -93,23 +58,12 @@ int _putfd(char c, int fd)
 		buf[i++] = c;
 	return (1);
 }
-<<<<<<< HEAD
 /**
  *_putsfd - prints an input string
  * @str: the string to be printed
  * @fd: the filedescriptor to write to
  * Return: the number of chars put
  **/
-=======
-
-/**
- * putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
- *
- * Return: the number of chars put
- */
->>>>>>> 7b5aa13d41c8b74dd11e3fc12bbd5fdd4a300253
 int _putsfd(char *str, int fd)
 {
 	int i = 0;
--- exits.c
+++ exits.c
@@ -5,6 +5,7 @@
  * @dest: the destination string to be copied to
  * @src: the source string
  * @n: the amount of characters to be copied
+ *Return: the concatenated string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -20,7 +21,7 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	if (i < n)
 	{
-		j= i;
+		j = i;
 		while (j < n)
 		{
 			dest[j] = '\0';
