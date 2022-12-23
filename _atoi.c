49a32fe267fd7dd872aad9e984ad35547b4c3164
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-12-23 15:00:27 -0800

    0

--- _atoi.c
+++ _atoi.c
@@ -15,7 +15,6 @@ int interactive(info_t *info)
  * is_delim - checks if character is a delimeter
  * @c: the char to check
  * @delim: the delimeter string
- *
  * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
@@ -23,13 +22,12 @@ int is_delim(char c, char *delim)
 	while (*delim)
 		if (*delim++ == c)
 			return (1);
-			return (0);
+	return (0);
 }
 
 /**
- * _isalpha - checks for alphabetic characte
+ * _isalpha - checks for alphabetic character
  * @c: The character to input
- *
  * Return: 1 if c is alphabetic, 0 otherwise
  */
 int _isalpha(int c)
@@ -41,20 +39,20 @@ int _isalpha(int c)
 }
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
- *
- * Return: 0 if no numbers in string, converted number otherwise
+ *  _atoi - converts a string to an integer
+ *  @s: the string to be converted
+ *  Return: 0 if no numbers in string, converted number otherwise
  */
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
-	
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
 			sign *= -1;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
@@ -64,9 +62,11 @@ int _atoi(char *s)
 		else if (flag == 1)
 			flag = 2;
 	}
+
 	if (sign == -1)
 		output = -result;
 	else
 		output = result;
+
 	return (output);
 }
