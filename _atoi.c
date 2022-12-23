d7f40d9c9ddc56dd6c019080e87534fc78a30092
Hana-aldaw <hana.m.aldaw@gmail.com>
2022-12-23 14:31:15 -0800

    0

--- _atoi.c
+++ _atoi.c
@@ -49,8 +49,8 @@ int _isalpha(int c)
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0
-
+	unsigned int result = 0;
+	
 	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
